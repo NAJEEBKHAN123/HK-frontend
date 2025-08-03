f659e883e9703854169aa57dc7349d3fe9ab0e94
NAJEEBKHAN123 <najeebkhanlaku@gmail.com>
2025-08-03 13:50:05 +0500

    chagne file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer()
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['axios', 'i18next'],
+          charts: ['chart.js', 'react-chartjs-2'],
+          pdf: ['jspdf', 'html2canvas']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    exclude: ['js-big-decimal'] // Add any other problematic dependencies
+  }
 })
