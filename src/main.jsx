65ed3aeb4e376b5863d47275737dacdf0ee76d48
NAJEEBKHAN123 <najeebkhanlaku@gmail.com>
2025-08-05 17:05:27 +0500

    add some seo comp

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
+    <HelmetProvider>
     <App />
+    </HelmetProvider>
     
   </StrictMode>,
 )
