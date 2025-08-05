ce4fd495dc8c1eb627a41ca2d33e796c562ab0d3
NAJEEBKHAN123 <najeebkhanlaku@gmail.com>
2025-08-05 17:19:00 +0500

    change

---
+++ src/components/SEO.jsx
@@ -0,0 +1,16 @@
+import { useDocumentHead } from 'react';
+
+export default function SEO({ title, description }) {
+  useDocumentHead({
+    title: title,
+    meta: [
+      { name: 'description', content: description },
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+      { name: 'twitter:title', content: title },
+      { name: 'twitter:description', content: description }
+    ]
+  });
+
+  return null;
+}
